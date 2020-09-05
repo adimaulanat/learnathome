703a1c4dbb66efb800ecb769132b7a377448e19a
Adi Maulana Triadi <adimaulanat@gmail.com>
2020-09-05 14:37:36 +0700

    Add new components are profile and post

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const username = 'adimaulanat'
-  return (
-    <>
+const Post = props => (
+  <img src={props.src} alt="profile picture" />
+)
+
+const Profile = (props) => (
+  <>
     <img src="https://via.placeholder.com/300" alt="profile picture" />
-    <div>@{username}</div>
+    <div>@{props.username}</div>
     <button>Follow</button>
     <div>Posts</div>
     <div>Follower</div>
     <div>Following</div>
     <div>Bio</div>
-    </>
+    <Post src="https://via.placeholder.com/300"/>
+  </>
+)
+
+function App() {
+  const username = 'adimaulanat'
+  return (
+    <Profile  username={username} />
   );
 }
 
