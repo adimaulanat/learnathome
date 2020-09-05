709c0303acf3a532c49d97c6d4cd39e62d7b7f85
Adi Maulana Triadi <adimaulanat@gmail.com>
2020-09-05 15:32:05 +0700

    Add styling for project

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
 
-const Post = props => (
-  <img src={props.src} alt="profile picture" />
-)
+const Post = ({ src }) => <img src={src} alt="profile picture" />;
 
 const Profile = (props) => {
+  const { username } = props;
   const [isFollowed, setIsFollowed] = useState(false);
+  const [keyword, setKeyword] = useState('');
+  const postCount = 5
+  const posts = [...Array(postCount)];
   return (
-    <>
-      <img src="https://via.placeholder.com/300" alt="profile picture" />
-      <div>@{props.username}</div>
-      <button onClick={() => setIsFollowed(!isFollowed)}>{isFollowed ? 'Unfollow' : 'Follow'}</button>
-      <div>Posts</div>
+    <React.Fragment>
+      <div>Search Result for account: {keyword}</div>
+      <input onChange={e => setKeyword(e.target.value)} value={keyword} placeholder="search profile" />
+      <img src="https://via.placeholder.com/150" alt="profile picture" />
+      <div>@{username}</div>
+      <button onClick={() => setIsFollowed(!isFollowed)}>
+        {isFollowed ? "Unfollow" : "Follow"}
+      </button>
+      <div>{postCount} Posts</div>
       <div>Follower</div>
       <div>Following</div>
       <div>Bio</div>
+      {postCount === 0 ? (
+        <div>No post</div>) : (
+          <div className="posts">
+            {posts.map((_, idx) => (
+              <Post
+                src="https://via.placeholder.com/300"
+                key={idx}
+                className="post"
+              />
+            ))}
+          </div>
+        )
+      }
       <Post src="https://via.placeholder.com/300" />
-    </>
-  )
-}
-
-function App() {
-  const username = 'adimaulanat'
-  return (
-    <Profile username={username} />
+    </React.Fragment>
   );
-}
+};
 
-export default App;
+export default function App() {
+  const username = "instagram";
+  return <Profile username={username} />;
+}
