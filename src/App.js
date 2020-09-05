1e40996e1fe9ee696a6f78436a2bc5e75bf87e31
Adi Maulana Triadi <adimaulanat@gmail.com>
2020-09-05 14:50:14 +0700

    Add function for follow button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,23 +6,26 @@ const Post = props => (
   <img src={props.src} alt="profile picture" />
 )
 
-const Profile = (props) => (
-  <>
-    <img src="https://via.placeholder.com/300" alt="profile picture" />
-    <div>@{props.username}</div>
-    <button>Follow</button>
-    <div>Posts</div>
-    <div>Follower</div>
-    <div>Following</div>
-    <div>Bio</div>
-    <Post src="https://via.placeholder.com/300"/>
-  </>
-)
+const Profile = (props) => {
+  const [isFollowed, setIsFollowed] = useState(false);
+  return (
+    <>
+      <img src="https://via.placeholder.com/300" alt="profile picture" />
+      <div>@{props.username}</div>
+      <button onClick={() => setIsFollowed(!isFollowed)}>{isFollowed ? 'Unfollow' : 'Follow'}</button>
+      <div>Posts</div>
+      <div>Follower</div>
+      <div>Following</div>
+      <div>Bio</div>
+      <Post src="https://via.placeholder.com/300" />
+    </>
+  )
+}
 
 function App() {
   const username = 'adimaulanat'
   return (
-    <Profile  username={username} />
+    <Profile username={username} />
   );
 }
 
